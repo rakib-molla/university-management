a7bd89ac907a54b5a3fd6bf50278c50ae20468cd
rakib-molla <rakibmail1997@gmail.com>
2023-12-10 20:47:02 +0600

    using transaction create student and user

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import config from '../../config/index';
 
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
@@ -6,6 +7,8 @@ import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
 import { generateStudentId } from './user.utils';
+import AppError from '../../errors/AppErrrors';
+import httpStatus from 'http-status';
 
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
@@ -25,21 +28,45 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
   // fond academic semester info
   const admissionSemester = await AcademicSemester.findById(payload.admissionSemester)
 
-  //set manually generated id
+  const session = await mongoose.startSession();
+
+  try{
+
+    session.startTransaction();
+
+    //set manually generated id
   userData.id = await generateStudentId(admissionSemester);
 
-  // create a user
-  const newUser = await User.create(userData);
+  // create a user (transaction-1)
+  const newUser = await User.create([userData], {session}); // array
 
   // create a student
-  if (Object.keys(newUser).length) {
-    // set id, _id as user
-    payload.id = newUser.id; // embedding id
-    payload.user = newUser._id; //reference id
-
-    const newStudent = await Student.create(payload);
-    return newStudent;
+  if (!newUser.length) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'fail to create user');
   }
+  // set id, _id as user
+  payload.id = newUser[0].id; // embedding id
+  payload.user = newUser[0]._id; //reference id
+
+  // create a user (transaction-2)
+  const newStudent = await Student.create([payload], {session});
+  
+  if(!newStudent){
+    throw new AppError(httpStatus.BAD_REQUEST, 'fail to create student');
+  }
+
+  await session.commitTransaction();
+  await session.endSession();
+
+  return newStudent;
+
+  }catch(error){
+    await session.abortTransaction();
+    await session.endSession();
+  }
+
+
+ 
 };
 
 export const UserServices = {
